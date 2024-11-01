b3984eaec246ac4bb60dd2ce4f990e921521f85a
Prasad-D-Ware <prasadware42116@gmail.com>
2024-11-01 21:04:11 +0530

    feat : footer text changed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <div className="w-2/3 mx-auto p-6 mt-5 border-t border-black text-center">Made with Love</div>
+    <div className="w-2/3 mx-auto p-6 mt-5 border-t border-black text-center">- <a href="https://x.com/prasadtwts" target="_blank" className="hover:underline">prasadware</a></div>
   )
 }
 
